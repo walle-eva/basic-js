63c587a173d13889d62dd05746893e8175b9ce34
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 22:40:19 +0700

    Update hanoi-tower.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,5 +1,5 @@
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-    throw 'Not implemented';
+    
     let turns = (2**disksNumber) -1
     let seconds = turns/(turnsSpeed/(3600));
     return {turns: turns, seconds: seconds}
