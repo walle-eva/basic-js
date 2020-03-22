9cd77e51c94bac21700d319fb1c1ef84bc74263f
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 22:54:41 +0700

    Update extended-repeater.js

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -1,5 +1,37 @@
-module.exports = function repeater(/* str, options */) {
-    throw 'Not implemented';
-    // remove line with error and write your code here
+module.exports = function repeater(str, options) {
+    if(options['repeatTimes'] === undefined){
+        options['repeatTimes'] = 1
+    }
+    if(options['additionRepeatTimes'] === undefined){
+        options['additionRepeatTimes'] = 1
+    }
+
+    if(options['addition'] !== undefined){
+        let str_add = '';
+        if(options['additionRepeatTimes'] === undefined || options['additionRepeatTimes'] == 1){
+            str_add = options['addition']
+        }
+        if(options['additionRepeatTimes'] > 1 && options['additionSeparator'] === undefined){
+            options['additionSeparator'] = '+';
+        }
+        if(options['additionRepeatTimes'] > 1){
+            let str_add_array = []
+            for (let i = 0; i < options['additionRepeatTimes']; i++) {
+                str_add_array.push(String(options['addition']))
+            }
+            str_add = str_add_array.join(options['additionSeparator'])
+        }
+        str += str_add
+    }
+
+    if(options['repeatTimes'] > 1 && options['separator'] === undefined){
+        options['separator'] = '+';
+    }
+    let out = []
+    for(let i =0; i<options['repeatTimes']; i++){
+        out.push(String(str))
+    }
+
+    return out.join(options['separator'])
 };
   
