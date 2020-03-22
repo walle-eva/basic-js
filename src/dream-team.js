262eea079e79d7de3d1c40be0b46f02b5131a742
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 13:49:25 +0700

    Update dream-team.js

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
+module.exports = function createDreamTeam(arr) {
   throw 'Not implemented';
-  // remove line with error and write your code here
+  arr = arr.sort();
+  let newArr = [];
+  for(let i = 0; i < arr.length; i++){
+    newArr.push(arr[i][0]);
+  }
+  let name = newArr.join('');
+  return name;
 };
