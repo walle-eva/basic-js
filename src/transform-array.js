e2d145a99f2ada0ff2b9a335700316a159571f9a
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 13:51:38 +0700

    Update transform-array.js

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,22 @@
-module.exports = function transform(/* arr */) {
+module.exports = function transform( arr ) {
     throw 'Not implemented';
-    // remove line with error and write your code here
+    let a = '--discard-next';
+  let b = '--discard-prev';
+  let c = '--double-next';
+  let d = '--double-prev';
+  for(let i = 0; i < arr.length; i++){
+    if(arr[i] == a){
+	  arr.splice(i, 2);
+	}
+	if(arr[i] == b){
+	  arr.splice(i - 1, 2);
+	}
+	if(arr[i] == c){
+	  arr.splice(i, 1, arr[i + 1]);
+	}
+	if(arr[i] == d){
+	  arr.splice(i - 1, 1, arr[i - 1]);
+	}
+  }
+  return arr;
 };
