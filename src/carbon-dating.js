3223a53762eddf47f7f9ae7c611326525cd3e431
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 13:48:40 +0700

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,5 +3,10 @@ const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(/* sampleActivity */) {
   throw 'Not implemented';
-  // remove line with error and write your code here
+  let k = MODERN_ACTIVITY/parseFloat(str);
+    let n = Math.log(k);
+    let t = 0.693/HALF_LIFE_PERIOD;
+    let time = n/t;
+    let result = Math.ceil(time);
+	return result;
 };
