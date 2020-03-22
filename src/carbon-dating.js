9757409b10c29c41edd84699c37fe79b26134aab
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 22:41:22 +0700

    Update carbon-dating.js

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -2,7 +2,7 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(str) {
-  throw 'Not implemented';
+
   let k = MODERN_ACTIVITY/parseFloat(str);
     let n = Math.log(k);
     let t = 0.693/HALF_LIFE_PERIOD;
