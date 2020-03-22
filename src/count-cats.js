d73ff75bbfaf33992ca7103010846555d168769b
walle-eva <walle-eva@users.noreply.github.com>
2020-03-22 22:49:19 +0700

    Update count-cats.js

--- src/count-cats.js
+++ src/count-cats.js
@@ -1,4 +1,14 @@
-module.exports = function countCats(/* matrix */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function countCats(matrix) {
+  let result = 0;
+  for (let x = 0; x < matrix.length; x++) {
+    const row = matrix[x];
+    for (let y = 0; y < row.length; y++) {
+      const cell = row[y];
+      if(cell == '^^')
+      {
+        result += 1;
+      }
+    }
+  }
+  return result;
 };
